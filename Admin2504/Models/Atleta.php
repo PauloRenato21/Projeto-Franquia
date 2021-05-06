00bbdab648575e6b433b28d071c9ea8a0b727a60
PauloRenato21 <paulorenato401@gmail.com>
2021-05-06 07:54:48 -0300

    add Admin2504 new query view info_atleta

--- Admin2504/Models/Atleta.php
+++ Admin2504/Models/Atleta.php
@@ -6,8 +6,7 @@ use Foundation\Database\Model;
 class Atleta extends Model {  
     
     public function getAllAtleta() {
-        $query = 'SELECT atleta.id, atleta.nome, cpf, dt_nascimento,endereco_rua,endereco_numero,endereco_bairro,endereco_CEP,naturalidade,telefone,email,turma.nome as turma_nome FROM atleta inner join turma on
-        atleta.fk_turma_id = turma.id';
+        $query = 'SELECT * FROM info_atleta';
         return $this->db->select($query);
     }
 
